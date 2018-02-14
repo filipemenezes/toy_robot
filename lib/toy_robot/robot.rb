3049978ea9d05c87a77591c67889d12bc2a4f1e1
Filipe Menezes <filipepmenezes@gmail.com>
2018-02-14 03:58:21 -0200

    :pencil: Add love to the API docs.

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -9,19 +9,19 @@ module ToyRobot
   # Examples
   #
   #   robot = Robot.new
-  #   robot.place 2, 2, :north
+  #   robot.place Position.new(Coordinate.new(2, 2), :north)
   #   robot.move
   #   robot.report
+  #   # => 2,3,NORTH
   class Robot
     # Public: Used to place the robot on the table in the given position.
     #
-    # x       - An integer representing the x coordinate.
-    # y       - An integer representing the y coordinate.
-    # facing  - A Symbol with the direction the robot is facing.
+    # position - A Position to define the current position an facing direction
+    # of a robot.
     #
     # Examples
     #
-    #   robot.place 2, 2, :north
+    #   robot.place Position.new(Coordinate.new(2, 2), :north)
     #
     # Returns nothing.
     def place(position)
@@ -30,20 +30,52 @@ module ToyRobot
     end
 
     # Public: Used to show the client where the Toy Robot is right now.
+    #
+    # Returns a String with the current position.
     def report
       @position.to_s
     end
 
+    # Public: Used to turn the robot 90˚ to the left (facing `Direction`).
+    #
+    # Examples
+    #
+    #   robot.place Position.new(Coordinate.new(2, 2), :north)
+    #   robot.left
+    #   robot.report
+    #   # => 2,2,WEST
+    #
+    # Returns nothing.
     def left
       raise NeedToBeInPlace.new unless @placed
       @position = @position.left
     end
 
+    # Public: Used to turn the robot 90˚ to the right (facing `Direction`).
+    #
+    # Examples
+    #
+    #   robot.place Position.new(Coordinate.new(2, 2), :north)
+    #   robot.rigth
+    #   robot.report
+    #   # => 2,2,EAST
+    #
+    # Returns nothing.
     def right
       raise NeedToBeInPlace.new unless @placed
       @position = @position.right
     end
 
+    # Public: Moves the robot one position in the current facing direction.
+    #
+    # Examples
+    #
+    #   robot.place Position.new(Coordinate.new(2, 2), :north)
+    #   robot.move
+    #   robot.report
+    #   # => 2,3,NORTH
+    #
+    # returns nothing.
     def move
       raise NeedToBeInPlace.new unless @placed
       @position = @position.forward
