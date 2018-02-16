2648b0e154346483af575b1115af5a3f7521b85b
Filipe Menezes <filipepmenezes@gmail.com>
2018-02-16 14:03:58 -0200

    Correct the size (axis size limit) comparison.

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -96,8 +96,8 @@ module ToyRobot
     private
 
     def outside_limits?(position)
-      (position.y < 0 || position.x < 0) ||
-        (position.y >= @table[0] || position.x >= @table[1])
+      (position.x < 0 || position.y < 0) ||
+        (position.x >= @table[0] || position.y >= @table[1])
     end
   end
 end
