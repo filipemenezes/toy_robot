4b8e6fdeb89f4edaa18aad0be2ca9c1390584e63
Filipe Menezes <filipepmenezes@gmail.com>
2018-02-19 14:41:44 -0300

    Add the command namespace.

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -4,6 +4,7 @@ require 'toy_robot/direction/all'
 require 'toy_robot/direction'
 require 'toy_robot/position'
 require 'toy_robot/coordinate'
+require 'toy_robot/command'
 
 module ToyRobot
 end
---
+++ lib/toy_robot/command.rb
@@ -0,0 +1,6 @@
+require 'toy_robot/command/place'
+require 'toy_robot/command/report'
+require 'toy_robot/command/register'
+
+module Command
+end
